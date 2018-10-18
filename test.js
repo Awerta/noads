4da439c3b97b585990448fde4a3dbb2eb2790789
Vahid Iravani <fr.vahidiravani@gmail.com>
2018-10-19 02:13:29 +0330

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 (function() {
 
-    document.getElementById("leftmenuinner").innerHTML = "Paragraph changed!";
+    document.getElementById("tab-android").innerHTML = "Paragraph changed!";
 
     // Your code here...
 })();
