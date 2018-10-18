6f993cbeff39e86f21789b212b5fef28dd6c37c0
Vahid Iravani <fr.vahidiravani@gmail.com>
2018-10-19 02:02:02 +0330

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 (function() {
 
-    document.getElementByClassName("acdb-image").innerHTML = "Paragraph changed!";
+    document.getElementByClassName("acdb").innerHTML = "Paragraph changed!";
 
     // Your code here...
 })();
