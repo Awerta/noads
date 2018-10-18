6cb2caa075eb2f382af1dfe4f8060a2f4beae35e
Vahid Iravani <fr.vahidiravani@gmail.com>
2018-10-19 02:02:49 +0330

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 (function() {
 
-    document.getElementByClassName("acdb").innerHTML = "Paragraph changed!";
+    document.getElementById("acdb-h1eader").innerHTML = "Paragraph changed!";
 
     // Your code here...
 })();
